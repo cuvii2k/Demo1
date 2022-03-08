a490fa46893db4952a5ed09b0de0b6d57c89387d
95vinhndu1415@gmail.com <95vinhndu1415@gmail.com>
2022-03-08 18:03:59 +0700

    commit for life

--- DemoLoginForm/DemoLoginForm/Form1.cs
+++ DemoLoginForm/DemoLoginForm/Form1.cs
@@ -17,5 +17,14 @@ namespace DemoLoginForm
             InitializeComponent();
         }
 
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
     }
 }
